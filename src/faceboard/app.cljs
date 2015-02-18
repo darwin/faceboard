d14f33905f985bde2d0f7e8dac6559de7dc15f31
Antonin Hildebrand <antonin@hildebrand.cz>
2015-02-18 23:48:03 +0700

    Move app state into a separate source file

--- src/faceboard/app.cljs
+++ src/faceboard/app.cljs
@@ -3,20 +3,8 @@
             [om-tools.core :refer-macros [defcomponent]]
             [om-tools.dom :as dom]
             [faceboard.utils :as utils :refer [log, log-err, log-warn]]
-            [faceboard.fixtures :as fixtures]
+            [faceboard.state :as state :refer [app-state]]
             [faceboard.tabs :as tabs]))
-
-(defonce app-state
-  (atom
-    {:model {:people fixtures/hackerparadise-people
-             :places fixtures/hackerparadise-places}
-     :ui    {:selected-tab-id :people
-             :tabs            [{:id :people :label "People"}
-                               {:id :places :label "Places"}]
-             :editing?        false
-             :source-editing? false
-             }
-     }))
 
 (defcomponent app-component [data owner opts]
   (render [_]
@@ -24,4 +12,4 @@
       (om/build tabs/tabs-component data))))
 
 (defn init! []
-  (om/root app-component app-state {:target (. js/document (getElementById "app"))}))
+  (om/root app-component app-state {:target (.getElementById js/document "app")}))
---
+++ src/faceboard/state.cljs
@@ -0,0 +1,15 @@
+(ns faceboard.state
+  (:require [faceboard.utils :as utils :refer [log, log-err, log-warn]]
+            [faceboard.fixtures :as fixtures]))
+
+(defonce app-state
+  (atom
+    {:model {:people fixtures/hackerparadise-people
+             :places fixtures/hackerparadise-places}
+     :ui    {:selected-tab-id :people
+             :tabs            [{:id :people :label "People"}
+                               {:id :places :label "Places"}]
+             :editing?        false
+             :source-editing? false
+             }
+     }))
