56bb12314eb3343c015ff4ece18b0bdf162da709
Antonin Hildebrand <antonin@hildebrand.cz>
2015-02-23 05:25:59 +0700

    Specialize logging in production mode

---
+++ frontend/src-prod/faceboard/logging.cljs
@@ -0,0 +1,12 @@
+(ns faceboard.logging)
+
+(defn- log-method [method & args]
+  (.apply (aget js/console method) js/console (into-array args)))
+
+(defn log [& _]) ; no-op in production mode
+
+(defn log-err [& args]
+  (apply log-method (cons "error" args)))
+
+(defn log-warn [& args]
+  (apply log-method (cons "warn" args)))
