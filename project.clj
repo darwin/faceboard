027f154ca599e2193960494f454c81db8585f4f8
Antonin Hildebrand <antonin@hildebrand.cz>
2015-02-23 06:10:24 +0700

    Bump ClojureScript dependency to "0.0-2913"

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+                 [org.clojure/clojurescript "0.0-2913"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om "0.8.8"]
                  [prismatic/om-tools "0.3.10"]
