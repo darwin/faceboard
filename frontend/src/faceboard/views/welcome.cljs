e341fad856c8490c2290cba99037c864df77de5a
Antonin Hildebrand <antonin@hildebrand.cz>
2015-02-25 12:46:40 +0700

    Implement simple welcome page

--- frontend/src/faceboard/views/welcome.cljs
+++ frontend/src/faceboard/views/welcome.cljs
@@ -1,10 +1,20 @@
 (ns faceboard.views.welcome
-  (:require [om-tools.core :refer-macros [defcomponent]]
+  (:require [om.core :as om]
+            [om-tools.core :refer-macros [defcomponent]]
+            [om-tools.dom :as dom]
             [faceboard.logging :refer [log, log-err, log-warn]]
+            [faceboard.controller :as controller]
             [faceboard.page :as page]))
 
 (defcomponent welcome-component [data _ _]
   (render [_]
     (let [ui (:ui data)]
       (page/page-skeleton
-        "WELCOME!"))))
+        (dom/div {:class "standard-page welcome"}
+          (dom/div {:class "big-logo"}
+            (dom/span {} "FACEBOARD"))
+          (dom/div {:class "teaser"}
+            (dom/img {:src "images/faceboard-teaser.jpg"}))
+          (dom/div {:class "buttons"}
+            (dom/span {:class "button"
+                       :on-click #(controller/perform-command! "create-board")} "Create a new board")))))))
