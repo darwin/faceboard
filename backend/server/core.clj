fe41a9700fb2e6540eb344e0f6ca1ba7f9bc2b19
Antonin Hildebrand <antonin@hildebrand.cz>
2015-03-07 16:19:08 +0700

    Improve server (add gzip and other goodies)

--- backend/server/core.clj
+++ backend/server/core.clj
@@ -1,12 +1,13 @@
 (ns server.core
   (:use compojure.core)
   (:require 
-    [compojure.handler :as handler :refer [site]]
+    [compojure.handler :refer [site]]
     [compojure.route :as route]
     [clojure.java.io :as io]
-    [ring.util.response :as resp]
     [ring.adapter.jetty :as jetty]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
     [clojure.data.json :as json]
+    [ring.middleware.gzip :refer [wrap-gzip]]
     [net.cgrand.enlive-html :refer [deftemplate, set-attr prepend append html]]
     [environ.core :refer [env]]))
 
@@ -15,17 +16,21 @@
 
 (def inject-env
   (comp
-    (prepend (html [:script {:type "text/javascript"} (str "window.faceboard_env = " (json/write-str client-side-env))]))
-    ))
+    (prepend (html [:script {:type "text/javascript"} (str "window.faceboard_env = " (json/write-str client-side-env))]))))
 
 (deftemplate index-page (io/resource (if production? "public/index-production.html" "public/index.html")) []
   [:body] inject-env)
 
-(defroutes app
+(defroutes app-routes
   (GET "/" req (index-page))
   (route/resources "/")
   (route/not-found "Not Found"))
 
+(def app (->
+           app-routes
+           (wrap-defaults site-defaults)
+           wrap-gzip))
+
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
-    (jetty/run-jetty (site #'app) {:port port :join? false})))
+    (jetty/run-jetty (var app) {:port port :join? false})))
