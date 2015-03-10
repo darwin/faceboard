63298647ee9817ddc26e88a84a2f3890b16325a1
Antonin Hildebrand <antonin@hildebrand.cz>
2015-03-10 20:37:37 +0700

    Bump clojurescript to 3058

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3030"]
+                 [org.clojure/clojurescript "0.0-3058"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om "0.8.8"]
                  [prismatic/om-tools "0.3.11"]
