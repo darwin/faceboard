9713b2794ceee92095194a7926d3cf21d41f5235
Antonin Hildebrand <antonin@hildebrand.cz>
2015-03-20 11:46:00 +0800

    Fix url matcher for hackerparadise

--- frontend/src/faceboard/whitelabel.cljs
+++ frontend/src/faceboard/whitelabel.cljs
@@ -5,7 +5,7 @@
 
 (def domain-mappings
   [[#"fb\.local.*" "test-localhost-whitelabel"]             ; just a test local host
-   [#"hackerparadise\.org" "hp"]])                          ; http://faceboard.hackerparadise.org
+   [#".*hackerparadise\.org" "hp"]])                          ; http://faceboard.hackerparadise.org
 
 (defn domain->boad-id [domain]
   (if-let [match (utils/find-first #(re-matches (first %) domain) domain-mappings)]
