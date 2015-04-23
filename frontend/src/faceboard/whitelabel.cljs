392c668afd0447cdd474b2136a77dee302c0c059
Antonin Hildebrand <antonin@hildebrand.cz>
2015-04-23 21:48:38 +0700

    Switch hacker-paradise board to less guessable id

--- frontend/src/faceboard/whitelabel.cljs
+++ frontend/src/faceboard/whitelabel.cljs
@@ -7,7 +7,7 @@
 
 (def domain-mappings
   [[#"fb\.local.*" "test-localhost-whitelabel"]             ; just a test local host
-   [#".*hackerparadise\.org" "hp"]])                        ; http://faceboard.hackerparadise.org
+   [#".*hackerparadise\.org" "3a8450f9a0cb45d99c1a"]])      ; http://faceboard.hackerparadise.org
 
 (defn domain->boad-id [domain]
   (if-let [match (utils/find-first #(re-matches (first %) domain) domain-mappings)]
