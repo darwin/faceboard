c41e01221392238872430a59fa4d5b266088fd33
Antonin Hildebrand <antonin@hildebrand.cz>
2015-05-09 19:39:16 +0700

    Bump clojure to 1.7.0-beta2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
   :jvm-opts ^:replace ["-Xms512m" "-Xmx512m" "-server"]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om "0.8.8" :exclusions [cljsjs/react]]
