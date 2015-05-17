095059834ff35dadee8a887f75f0489dbcee7de0
Antonin Hildebrand <antonin@hildebrand.cz>
2015-05-17 15:20:29 +0800

    Fix migration to full urls

--- frontend/src/faceboard/migrations/m003_socials_to_full_links.cljs
+++ frontend/src/faceboard/migrations/m003_socials_to_full_links.cljs
@@ -10,17 +10,19 @@
       :else [(str/lower (first parts)) (str/lower (second parts))])))
 
 (defn build-known-url [type id]
-  (condp #(str/starts-with? %2 %1) type                     ; recognized urls from id
-    "facebook" (str "https://www.facebook.com/" id)
-    "twitter" (str "https://twitter.com/" id)
-    "github" (str "https://github.com/" id)
-    "linkedin" (str "https://www.linkedin.com/in/" id)
-    "flickr" (str "https://www.flickr.com/people/" id)
-    "lastfm" (str "http://www.last.fm/user/" id)
-    "hacker-news" (str "https://news.ycombinator.com/user?id=" id)
-    "reddit" (str "http://www.reddit.com/user/" id)
-    "instagram" (str "https://instagram.com/" id)
-    nil))
+  (if (str/starts-with? id "http")
+    id
+    (condp #(str/starts-with? %2 %1) type                   ; recognized urls from id
+      "facebook" (str "https://www.facebook.com/" id)
+      "twitter" (str "https://twitter.com/" id)
+      "github" (str "https://github.com/" id)
+      "linkedin" (str "https://www.linkedin.com/in/" id)
+      "flickr" (str "https://www.flickr.com/people/" id)
+      "lastfm" (str "http://www.last.fm/user/" id)
+      "hacker-news" (str "https://news.ycombinator.com/user?id=" id)
+      "reddit" (str "http://www.reddit.com/user/" id)
+      "instagram" (str "https://instagram.com/" id)
+      nil)))
 
 (defn- convert-parts [[type id]]
   (or (build-known-url type id) id))
